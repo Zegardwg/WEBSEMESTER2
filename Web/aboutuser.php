8913d8094cd2947a03ba341794ea07cbfe6e1926
AuliaDiva1 <161435778+AuliaDiva1@users.noreply.github.com>
2024-05-24 22:31:48 +0700

    Create aboutuser.php

---
+++ Web/aboutuser.php
@@ -0,0 +1,369 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!--font google-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
+
+
+    <!--style-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!--logo title-->
+    <link rel="icon" href="assets/images/logo.png" type="image/x-icon-dark">
+    <title>Trans Madiun</title>
+  </head>
+  <body id="home">
+    <!--navbar-->
+    <nav class="navbar navbar-expand-lg navbar-light position-fixed w-100" style="background-color: #ffffff;">
+        <div class="container">
+          <a class="navbar-brand" href="utama.php">
+            <img src="assets/images/logo1.png" class="brand-transparent my-0" alt="" width="90" height="1400">
+          </a>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+              <!-- <li class="nav-item"> -->
+              <a class="nav-link active" aria-current="page" href="home.php">Beranda</a>
+              <a class="nav-link "  href="#order">Cara Order</a>
+              <a class="nav-link "  href="#jadwal">Jadwal</a>
+              <a class="nav-link "  href="#about">Layanan</a>
+              <a class="nav-link "  href="aboutuser.php">Tentang Kami</a>
+
+            </div>
+
+                <style>
+        .navbar {
+            background-color: #ffffff;
+            padding: 5px 0; /* Kurangi padding untuk membuat navbar lebih tipis */
+        }
+        .navbar .navbar-brand img {
+            height: 50px; /* Kurangi tinggi logo agar navbar lebih tipis */
+        }
+        .navbar-nav .nav-link {
+            padding: 5px 10px; /* Kurangi padding pada link navbar */
+        }
+        .auth-links {
+            text-align: center;
+            margin: 10px 0; /* Kurangi margin untuk membuatnya lebih tipis */
+        }
+        .auth-links a {
+            display: inline-block;
+            margin: 0 10px;
+            padding: 5px 15px; /* Kurangi padding pada link auth */
+            border-radius: 20px;
+            background-color: #007bff;
+            color: #fff;
+            text-decoration: none;
+        }
+        .auth-links a:hover {
+            background-color: #0056b3;
+        }
+    </style>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+          <!--section Visi Misi-->
+          <div id="content" class="site-content" style="padding-top: 50px; padding-left: 20px;">
+    <div class="ast-container">
+        <div id="primary" class="content-area primary">
+            <main id="main" class="site-main">
+                <article class="post-3973 post type-post status-publish format-image hentry category-profil post_format-post-format-image ast-article-single" id="post-3973" itemscope itemtype="https://schema.org/CreativeWork">
+                    <div class="ast-post-format-standard ast-no-thumb single-layout-1">
+                        <header class="entry-header ast-no-title ast-no-thumbnail">
+                        <div class="entry-content clear" itemprop="text" style="padding-top: 20px; padding-left: 20px;">
+                            <div class="wp-block-uagb-container uagb-block-f9e4482c alignfull uagb-is-root-container">
+                                <div class="uagb-container-inner-blocks-wrap">
+                                    <div class="wp-block-uagb-container uagb-block-7c8dd61e" style="margin-top: 20px; margin-left: 20px;">
+                                        <div class="wp-block-uagb-advanced-heading uagb-block-22c2b4bd">
+                                            <h3 class="uagb-heading-text">Visi & Misi</h3>
+                                        </div>
+                                        <div class="wp-block-uagb-advanced-heading uagb-block-95179e45">
+                                            <h class="uagb-heading-text"><strong>Visi</strong></h5>
+                                            <p class="uagb-desc-text">Terwujudnya Lalu Lintas dan Angkutan Jalan yang tertib, lancar, selamat, serta pelayanan transportasi berbasis Teknologi.</p>
+                                        </div>
+                                        <div class="wp-block-uagb-advanced-heading uagb-block-88c52e1b">
+                                            <h class="uagb-heading-text"><strong>Misi</strong></h5>
+                                            <p class="uagb-desc-text">Meningkatkan kemampuan SDM Perhubungan, ketersediaan sarana, prasarana dan fasilitas keselamatan transportasi serta keterlibatan peran serta masyarakat dalam bidang LLAJ. Mewujudkan pengembangan teknologi informasi dalam mendukung pelayanan transportasi yang handal.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </main>
+        </div>
+    </div>
+</div>
+
+          <!--section Visi Misi-->
+          
+
+
+          <!--start footer-->
+          
+          <footer>
+
+<div class="container__footer">
+    <div class="box__footer">
+        <div class="terms">
+            <p>Trans Madiun Merupakan salah satu sistem transportasi publik yang beroperasi di Kabupaten Madiun, Jawa Timur.</p>
+      </div>
+    </div>
+    <div class="box__footer">
+        <h2>Sosial Media</h2>
+        <a href="https://www.facebook.com/"> <i class="fab fa-facebook-square"></i> Facebook</a>
+        <a href="https://twitter.com/login"><i class="fab fa-twitter-square"></i> Twitter</a>
+        <a href="https://www.tiktok.com"><i class="fab fa-tiktok"></i> Tiktok</a>
+        <a href="https://www.instagram.com/"><i class="fab fa-instagram-square"></i> Instagram</a>
+    </div>
+
+    <div class="box__footer">
+        <h2>Trans Madiun</h2>
+        <a href="#home">Beranda</a>
+        <a href="#order">Cara order</a>
+        <a href="#jadwal">Jadwal</a>
+        <a href="#layanan">Layanan</a>
+        <a href="#about">Tentang Kami</a>              
+    </div>
+
+</div>
+
+<div class="box__copyright">
+    <hr>
+    <p>TransMadiun copyright @2024 <b>TransMadiun</b></p>
+</div>
+<style>
+  footer{
+    width: 100%;
+    padding: 50px 0px;
+    background-image: url(assets/images/sec_jadwal.png);
+    background-size: cover;
+    
+    /*background-color: #d0f0f8;
+    -webkit-mask-image: url("../Images/background-footer.svg");
+    mask-image: url("../Images/background-footer.svg");
+    -webkit-mask-size: cover;
+    mask-size: cover;*/
+}
+
+.container__footer{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    max-width: 1200px;
+    margin: auto;
+    margin-top: 100px;
+}
+
+.box__footer{
+    display: flex;
+    flex-direction: column;
+    padding: 40px;
+}
+
+.box__footer .logo img{
+    width: 180px;
+}
+
+.box__footer .terms{
+    max-width: 350px;
+    margin-top: 20px;
+    font-weight: 500;
+    color: #7a7a7a;
+    font-size: 18px;
+}
+
+.box__footer h2{
+    margin-bottom: 30px;
+    color: #343434;
+    font-weight: 700;
+}
+
+.box__footer a{
+    margin-top: 10px;
+    color: #7a7a7a;
+    font-weight: 600;
+}
+
+.box__footer a:hover{
+    opacity: 0.8;
+}
+
+.box__footer a .fab{
+    font-size: 20px;
+}
+
+.box__copyright{
+    max-width: 1200px;
+    margin: auto;
+    text-align: center;
+    padding: 0px 40px;
+    color :black;
+}
+
+.box__copyright p{
+    margin-top: 20px;
+    color: #7a7a7a;
+}
+
+.box__copyright hr{
+    border: none;
+    height: 1px;
+    background-color: #7a7a7a;
+}
+</style>
+</footer>
+
+
+          <!--start footer-->
+
+        <style>
+.form-request-wrap-modern {
+ background-color: #f5f5f5;
+ padding: 50px;
+ border-radius: 10px;
+ }
+.form-request-modern {
+ background-color: #fff;
+ padding: 30px;
+ border-radius: 10px;
+ }
+.form-request-modern h4 {
+ margin-bottom: 30px;
+ }
+.form-request-modern.form-input {
+ margin-bottom: 20px;
+ }
+.form-request-modern.form-button {
+ margin-top: 30px;
+ }
+ .rd-mailform {
+  margin-top: 30px;
+}
+
+.form-label-outside {
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  display: block;
+}
+
+.form-wrap {
+  margin-bottom: 20px;
+}
+
+.select-filter {
+  width: 100%;
+  padding: 10px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="none" stroke="%23ccc" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2 5l6 6 6-6"/></svg>');
+  background-repeat: no-repeat;
+  background-position: right 10px center;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+
+.select-filter:focus {
+  outline: none;
+  border-color: #666;
+}
+
+.select-filter::-ms-expand {
+  display: none;
+}
+.rd-mailform {
+  margin-top: 30px;
+}
+
+.form-label-outside {
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  display: block;
+}
+
+.form-wrap {
+  margin-bottom: 20px;
+}
+
+.form-wrap-inline {
+  display: inline-block;
+  width: 100%;
+}
+
+.form-input {
+  width: 100%;
+  padding: 10px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+}
+
+.select-filter {
+  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="none" stroke="%23ccc" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2 5l6 6 6-6"/></svg>');
+  background-repeat: no-repeat;
+  background-position: right 10px center;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+}
+
+.select-filter:focus {
+  outline: none;
+  border-color: #666;
+}
+
+.select-filter::-ms-expand {
+  display: none;
+}
+
+/* Add some space between the two select elements */
+.col-sm-12:nth-child(2) {
+  margin-top: 20px;
+}
+.swiper-slide {
+  position: relative;
+}
+
+.swiper-slide-caption {
+  position: absolute;
+  top: 90%;
+  left: 45%;
+  transform: translate(-50%, 85%);
+  text-align: center;
+  color: #fff; /* or any other color you prefer */
+}
+
+
+/* TESSSSS */
+
+</style>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
