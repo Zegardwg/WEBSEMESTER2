10963c56b3a76de8d4bcf073f0f6e081ef982c8d
AuliaDiva1 <161435778+AuliaDiva1@users.noreply.github.com>
2024-05-24 22:32:29 +0700

    Update register.php

--- Web/register.php
+++ Web/register.php
@@ -43,7 +43,7 @@ $background_image = "assets/images/sec_jadwal.png";
   <div class="register-box-body">
     <p class="login-box-msg">Register a new membership</p>
 
-    <form action="prosesregis.php" method="post">
+    <form method="post" enctype="multipart/form-data" action="prosesregis.php">
       <div class="form-group has-feedback">
         <input type="text" name="nisn" class="form-control" placeholder="NISN">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
@@ -80,6 +80,10 @@ $background_image = "assets/images/sec_jadwal.png";
         <input type="password" class="form-control" name="pass_user" placeholder="Password">
         <span class="glyphicon glyphicon-phone form-control-feedback"></span>
       </div>
+      <div class="form-group has-feedback">
+        <input type="file" name="foto" id="foto" class="form-control" placeholder="Foto" required>
+        <span class="glyphicon glyphicon-camera form-control-feedback"></span>
+      </div>
       <div class="row">
         <div class="col-xs-8">
           <div class="checkbox icheck">
@@ -90,7 +94,7 @@ $background_image = "assets/images/sec_jadwal.png";
         </div>
         <!-- /.col -->
         <div class="col-xs-4">
-          <button type="submit" name="register" class="btn btn-primary btn-block btn-flat">Register</button>
+          <button type="submit"  name="register" value="Register" class="btn btn-primary btn-block btn-flat">Register</button>
         </div>
         <!-- /.col -->
         </div>
